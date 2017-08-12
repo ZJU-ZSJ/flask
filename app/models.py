f22c413676746983d474147708b1c692b4b546a7
ZhangShijie <3160102448@zju.edu.cn>
2017-08-12 09:17:51 +0800

    1

--- app/models.py
+++ app/models.py
@@ -54,10 +54,13 @@ class Article(db.Model):
     def on_changed_body(target, value, oldvalue, initiator):
         allowed_tags = ['a', 'abbr', 'acronym', 'b', 'blockquote', 'code',
                         'em', 'i', 'li', 'ol', 'pre', 'strong', 'ul',
-                        'h1', 'h2', 'h3', 'p']
+                        'h1', 'h2', 'h3', 'p','img']
+        attrs = {
+            'img': ['src', 'alt']
+        }
         target.body_html = bleach.linkify(bleach.clean(
             markdown(value, output_format='html'),
-            tags=allowed_tags, strip=True))
+            tags=allowed_tags, strip=True, attributes=attrs))
 
 db.event.listen(Article.body, 'set', Article.on_changed_body)
 
